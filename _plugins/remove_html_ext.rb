dd7687d8cde205bab730140acae5d81f54d90ce4
jcxia43 <xjconlyme@gmail.com>
2019-07-03 14:58:29 +0800

    Update plugin

--- _plugins/remove_html_ext.rb
+++ _plugins/remove_html_ext.rb
@@ -9,3 +9,12 @@ Jekyll::Hooks.register :posts, :post_write do |post|
     FileUtils.mv(path, path.sub(/\.html$/, ''))
   end
 end
+
+Jekyll::Hooks.register :pages, :post_write do |page|
+  if Jekyll.env == 'production'
+    path = page.destination('/')
+    if path =~ /\.html$/ && !path =~ /404\.html/ && !path =~ /index\.html/
+      FileUtils.mv(path, path.sub(/\.html$/, ''))
+    end
+  end
+end
